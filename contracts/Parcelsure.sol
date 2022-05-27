f5d34240ff50af18dcd311839b01efb411bdbaba
Kai Morciniec <kaimorciniec@gmail.com>
2022-05-27 18:56:25 +0100

    add keeper imports

--- contracts/Parcelsure.sol
+++ contracts/Parcelsure.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-// import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
 /* ERRORS */
