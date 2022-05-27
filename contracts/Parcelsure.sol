6e565019a4907d8ec07a3464fc3e78defc01810d
Kai Morciniec <kaimorciniec@gmail.com>
2022-05-27 03:35:43 +0100

    add cl client contract import

--- contracts/Parcelsure.sol
+++ contracts/Parcelsure.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.7;
 
 // import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "hardhat/console.sol";
 
 /* ERRORS */
