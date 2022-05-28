43cca60eedd307a6ae758697965c5f3064f3559b
Kai Morciniec <kaimorciniec@gmail.com>
2022-05-28 01:49:11 +0100

    add contract store

--- interface/src/lib/stores/ethers.ts
+++ interface/src/lib/stores/ethers.ts
@@ -1,5 +1,6 @@
 import { ethers, BigNumber } from 'ethers'
 import { writable, derived, type Writable } from 'svelte/store'
+import contractAbi from "$lib/abis/Parcelsure.json"
 
 export const account: Writable<string> = writable()
 export const provider = derived(account, ($account) => {
@@ -7,6 +8,10 @@ export const provider = derived(account, ($account) => {
 		return new ethers.providers.Web3Provider(window.ethereum);
 	}
 })
-
+export const Parcelsure = derived(provider, $provider => {
+    if (!$provider) return null;         
+    const signer = $provider.getSigner();
+    return new ethers.Contract("", contractAbi, signer)
+})
 //Metamask ETH balance
 export const walletBalance = writable();
